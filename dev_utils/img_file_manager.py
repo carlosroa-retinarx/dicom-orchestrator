1182d55c52f037734fa60ff817d451d1c624a236
OsmaniCR94 <ocasanueva@xsolution.cl>
2022-08-31 11:11:16 -0400

    img_file_manager actualizado

--- dev_utils/img_file_manager.py
+++ dev_utils/img_file_manager.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pydicom
 from PIL import Image
 import tempfile
 from pydicom.filewriter import dcmwrite
@@ -8,40 +9,19 @@ from pydicom.dataset import FileDataset, FileMetaDataset
 from pydicom.uid import UID
 from datetime import date, datetime
 from dotenv import load_dotenv
-import base64
+import cv2
+
 
 load_dotenv()
 
 
-def to_binary(img_path):
-    """
-    Transform a file to binary
-    :param img_path: str with img path
-    :return: binary file
-    """
-    with open(img_path, 'rb') as f:
-        return f.read()
-
-
-def base64_encode(img_path):
-    with open(img_path, "rb") as f:
-        return base64.b64encode(f.read())
-    # with open(img_path, "rb") as f:
-    #     return base64.b64encode(f.read())
-
-
-def base64_decode(b64_string):
-    return b64_string.decode('utf-8')
-
-
-def dcm_format_to(img_path: str, format='png', output: bool = False):
+def dcm_format_to(img_path: str, format='png'):
     """
     Convert a dcm image
     Example: file_name = dcm_format('my_dicom_file.dcm', 'png')
-    :param output: if True return img file converted
     :param img_path: str with the image path
     :param format: format to convert
-    :returns: img file converted
+    :returns: Filename
     :exception: Image not found
     :exception: Format must be a string
     """
@@ -55,7 +35,6 @@ def dcm_format_to(img_path: str, format='png', output: bool = False):
             im = dcmread(img_path.replace('/', '\\'))
         except:
             raise Exception("Image not found")
-
     filename = im.filename
     # Convert into a pixel array
     im = im.pixel_array.astype(float)
@@ -75,22 +54,20 @@ def dcm_format_to(img_path: str, format='png', output: bool = False):
     else:
         raise Exception("Format must be a string")
 
-    if output is True:
-        return final_image
+    return file_name
 
 
 # x = dcm_format_to("RG1_JLSN.dcm")
 # print(x)
 
 
-def format_to_dcm(img_path: str, img_name: str, output: bool = False):
+def format_to_dcm(img_path: str, img_name: str, fileid: str):
     """
         Convert an image to dcm
         Example: file_name = dcm_format('my_dicom_file.dcm', 'png')
-        :param output: if True return img file converted
         :param img_name: str with file's name
         :param img_path: str with the image path
-        :returns: img file converted into dcm format
+        :returns: Filename
         :exception: Image not found
         :exception: Format must be a string
         """
@@ -99,89 +76,106 @@ def format_to_dcm(img_path: str, img_name: str, output: bool = False):
     suffix = '.dcm'
     temp_file = tempfile.NamedTemporaryFile(suffix=suffix).name
 
+    indexnumbertemp = fileid.split('_')
+    print(indexnumbertemp)
+    indexnumber = indexnumbertemp[len(indexnumbertemp) - 1]
+
+    sop_instance_uid = "1.2.826.0.1.3680043.10.1026.1." + indexnumber
+
     # Populate required values for file meta information
     file_meta = FileMetaDataset()
+    file_meta.FileMetaInformationGroupLength = 190
+    file_meta.FileMetaInformationVersion = b'\x00\x01'
     file_meta.MediaStorageSOPClassUID = UID('1.2.840.10008.5.1.4.1.1.1')  # ComputedRadiographyImageStorage
-    file_meta.MediaStorageSOPInstanceUID = UID("1.2.3")
-    file_meta.ImplementationClassUID = UID("1.2.3.4")
+    file_meta.MediaStorageSOPInstanceUID = UID(sop_instance_uid)
+    file_meta.TransferSyntaxUID = UID('1.2.840.10008.1.2.1')  # Explicit VR Little Endian
+    file_meta.ImplementationClassUID = UID("1.2.826.0.1.3680043.10.1026")
+    file_meta.ImplementationVersionName = "retinarx-1.0"
 
     # Create the FileDataset instance (initially no data elements, but file_meta supplied)
     ds = FileDataset(temp_file, {}, file_meta=file_meta, preamble=b"\0" * 128)
+    print("DS", ds)
 
     # Add the data elements -- not trying to set all required here. Check DICOM standard
     ####### Aquí van valores pasados a la funcion con la metadata relacionada a la imagen #######
-    patient_name = "Test^Firstname"
+    patient_name = "RetinaRX^Dataset01-" + fileid
     ds.PatientName = patient_name
-    patient_id = "123456"
+    patient_id = fileid + indexnumber
     ds.PatientID = patient_id
+    ds.Modality = "CR"
+    ds.StudyInstanceUID = UID("1.2.826.0.1.3680043.10.1026.2." + indexnumber)
+    ds.SeriesInstanceUID = UID("1.2.826.0.1.3680043.10.1026.2.1." + indexnumber)
+    ds.SOPInstanceUID = UID(sop_instance_uid)
+    ds.SOPClassUID = UID("1.2.840.10008.5.1.4.1.1.1")
+    ds.StudyDate = "20220827"
+    ds.AccessionNumber = "100" + indexnumber
+    ds.StudyID = indexnumber
+    ds.SeriesNumber = "1"
+    ds.InstanceNumber = "1"
+    ds.BodyPartExamined = "CHEST"
+    ds.PixelSpacing = [1, 1]
+    ds.PatientBirthDate = "19720827"
+    ds.PatientSex = "M"
+    ds.StudyDescription = "Test"
+    ds.PatientAge = "050Y"
 
     # Set the transfer syntax
     ds.is_little_endian = True
-    ds.is_implicit_VR = True
+    ds.is_implicit_VR = False
 
     # Set creation date/time
     dt = datetime.now()
     ds.ContentDate = dt.strftime('%Y%m%d')
     timeStr = dt.strftime('%H%M%S.%f')  # long format with micro seconds
-    ds.ContentTime = timeStr
+    # ds.ContentTime = timeStr
     date_to_filename = dt.strftime("%m-%d-%Y_%H-%M-%S")
 
     # Image Pgn Info
     try:
         # windows
-        img = Image.open(f'{img_path}//{img_name}')  # the PNG or JPG file to be replace
+        cv2img = cv2.imread(f'{img_path}//{img_name}', -1)  # the PNG or JPG file to be transformed
+
     except:
         # linux
-        img = Image.open(f'{img_path}\\{img_name}')  # the PNG or JPG file to be replace
+        cv2img = cv2.imread(f'{img_path}', -1)  # the PNG or JPG file to be transformed
 
-    if img.mode == 'L':
-        np_image = np.array(img.getdata(), dtype=np.uint8)
-        ds.Rows = img.height
-        ds.Columns = img.width
-        ds.PhotometricInterpretation = "MONOCHROME1"
-        ds.SamplesPerPixel = 1
-        ds.BitsStored = 8
-        ds.BitsAllocated = 8
-        ds.HighBit = 7
-        ds.PixelRepresentation = 0
-        ds.PixelData = np_image.tobytes()
-        ds.is_little_endian = True
-        ds.is_implicit_VR = True
+    cv2img = cv2.resize(cv2img, (512, 512))
+    # cv2.imshow("cv2img", cv2img)
+    # cv2.waitKey()
 
-    elif img.mode == 'RGBA':
-        np_image = np.array(img.getdata(), dtype=np.uint8)[:, :3]
-        ds.Rows = img.height
-        ds.Columns = img.width
-        ds.PhotometricInterpretation = "RGB"
-        ds.SamplesPerPixel = 3
-        ds.BitsStored = 8
-        ds.BitsAllocated = 8
-        ds.HighBit = 7
-        ds.PixelRepresentation = 0
-        ds.PixelData = np_image.tobytes()
-        ds.is_little_endian = True
-        ds.is_implicit_VR = True
+    ds.Rows = cv2img.shape[0]
+    ds.Columns = cv2img.shape[1]
+    ds.PhotometricInterpretation = "MONOCHROME2"
+    ds.SamplesPerPixel = 1
+    ds.BitsStored = 16
+    ds.BitsAllocated = 16
+    ds.HighBit = 15
+    ds.PixelRepresentation = 0
+    ds.PixelData = cv2img.tobytes()
 
-    print("Writing test file", temp_file)
-    ds.save_as(temp_file)
-    print("File saved.")
+    pydicom.dataset.validate_file_meta(ds.file_meta, enforce_standard=True)
 
     # Create the dicom file
-    file_name = f'{patient_name}_{patient_id}_{date_to_filename}{suffix}'
-    dcmwrite(filename=file_name, dataset=ds)
+    dcmwrite(filename=f'./inDICOM_padchest_sample2/{patient_id}{suffix}', dataset=ds)
 
     print('Load file {} ...'.format(temp_file))
-    ds = dcmread(temp_file)
-    # print(ds)
-
-    # Remove the created file
-    print('Remove file {} ...'.format(temp_file))
-    os.remove(temp_file)
-
-    if output is True:
-        return ds
+    print(ds)
 
 
-# x = format_to_dcm('.', 'RG1_JLSN_2022-08-25.png', output=True)
-# print(x)
-
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(os.getcwd())
+    names = set()
+    i = 0
+    for f in os.listdir('./padchest_sample2'):
+        i = i + 1
+        name = f.split('.')[0].split("_")[1]
+        if name not in names:
+            name = name + "_" + str(i)
+            names.add(name)
+            format_to_dcm("./padchest_sample2", f, name)
+        else:
+            name = name + "_" + f.split('.')[0].split("_")[2]
+            name = name + "_" + str(i)
+            names.add(name)
+            format_to_dcm("./padchest_sample2", f, name)
--- routes/images.py
+++ routes/images.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Request
-from dev_utils.img_file_manager import base64_encode, base64_decode
 from db_conn.psql_connector import Cursor
 
 
@@ -19,11 +18,11 @@ async def save_image():
     con = Cursor().con
     con.autocommit = True
     cursor = Cursor().cursor()
-    query = """INSERT INTO dcm_study (desc_dcm_study, study_instance_uid, image_file, procesable) VALUES (%s, %s, %s, %s) returning cod_dcm_study"""
-    # query = """SELECT * FROM dcm_study"""
-    cursor.execute(
-        query,
-        ('TEST1', 'bbbbb', base64_encode('./dev_utils/RG1_JLSN.dcm'), 1)
-    )
-    data = cursor.fetchone()[0]
-    return data
+    # query = """INSERT INTO dcm_study (desc_dcm_study, study_instance_uid, image_file, procesable) VALUES (%s, %s, %s, %s) returning cod_dcm_study"""
+    # # query = """SELECT * FROM dcm_study"""
+    # cursor.execute(
+    #     query,
+    #     ('TEST1', 'bbbbb', base64_encode('./dev_utils/RG1_JLSN.dcm'), 1)
+    # )
+    # data = cursor.fetchone()[0]
+    return "data"
